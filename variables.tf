0ffc15490567dde76b5846f1b51f7a9b32a19580
Ubuntu <ubuntu@ip-172-31-40-233.eu-central-1.compute.internal>
2026-05-13 20:05:58 +0000

    Improve Terraform variable definitions

--- variables.tf
+++ variables.tf
@@ -1,11 +1,17 @@
 variable "aws_region" {
+	description = "AWS region"
+	type = string
 	default = "eu-central-1"
 }
 
 variable "instance_type" {
+	description = "EC2 instance type"
+	type = string
 	default = "t3.micro"
 }
 
 variable "ami_id" {
+	description = "AMI ID for EC2 instance"
+	type = string
 	default = "ami-0e872aee57663ae2d"
 }
