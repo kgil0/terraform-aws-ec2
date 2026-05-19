9fbb40db83862e952a4ffe3c8125b60b89a50eb7
Ubuntu <ubuntu@ip-172-31-40-233.eu-central-1.compute.internal>
2026-05-19 16:39:43 +0000

    Add EC2 user data nginx setup

--- main.tf
+++ main.tf
@@ -1,40 +1,49 @@
 provider "aws" {
-	region = var.aws_region
+  region = var.aws_region
 }
 
 resource "aws_security_group" "terraform_sg" {
-	name = "terraform-sg"
+  name = "terraform-sg"
 
-	ingress {
-		from_port	= 22
-		to_port		= 22
-		protocol	= "tcp"
-		cidr_blocks	= ["0.0.0.0/0"]
-	}
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-	ingress {
-		from_port	= 80
-		to_port		= 80
-		protocol	= "tcp"
-		cidr_blocks	= ["0.0.0.0/0"]
-	}
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-	egress {
-		from_port	= 0
-		to_port		= 0
-		protocol	= "-1"
-		cidr_blocks	= ["0.0.0.0/0"]
-	}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "terraform_test" {
-	ami     = var.ami_id
-	instance_type = var.instance_type
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
-	vpc_security_group_ids = [aws_security_group.terraform_sg.id]
+  vpc_security_group_ids = [aws_security_group.terraform_sg.id]
 
-	tags = {
-		Name = "terraform-test"
-	}
+  user_data = <<-EOF
+	#!/bin/bash
+	apt update -y
+	apt install nginx -y
+	systemctl enable nginx
+	systemctl start nginx
+	echo "Hello from Terraform EC2" > /var/www/html/index.html
+	EOF	
+
+  tags = {
+    Name = "terraform-test"
+  }
 }
 
